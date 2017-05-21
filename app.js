ea59f551cda8bede466ec2f1f2982ca81d3cfadf
ganpt <ganpatchoudhary.28@gmail.com>
2017-05-21 19:49:42 +0530

    added error and success messages for signin and signup

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
-var express=require('express');
-var ejs=require('ejs');
-var path=require('path');
-var bodyParser=require('body-parser');
-var session=require('express-session');
+var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var expressLayouts = require('express-ejs-layouts');
-var routes=require('./routes/index');
+var routes = require('./routes/index');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var flash=require('connect-flash');
 var LocalStrategy = require('passport-local').Strategy;
 mongoose.connect('mongodb://localhost/ShoppingOwn');
-var app=express();
+var app = express();
 //app.set('views', path.join(__dirname, 'views'));
-app.use(session(
-  {secret:'secretkey',resave: false,
-   saveUninitialized: true}));
+app.use(session({
+  secret: 'secretkey',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/',routes);
-app.listen(3000,function () {
+app.use(flash());
+/*app.use(function(req,res,next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+//  console.log(res.locals.error);
+  next();
+})*/
+app.use('/', routes);
+app.listen(3000, function() {
   console.log("server listening at port 3000")
 });
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var router = express.Router();
 //console.log("hi");
 router.use(csrf());
-//middleware which can be added on any page which requires authentication 
+//middleware which can be added on any page which requires authentication
 /*function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
@@ -20,36 +20,40 @@ router.use(csrf());
 };*/
 
 passport.serializeUser(function (user, done) {
+//  console.log("serializeUser user id is :",user.id);
     done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
+//  console.log("deserializeUser user id is :",id);
     User.findById(id, function (err, user) {
         done(err, user);
     });
 });
 passport.use(new LocalStrategy({
   usernameField: 'email',
-  passwordField: 'password'
-},function(email, password, done) {
+  passwordField: 'password',
+  passReqToCallback:true
+},function(req,email,password,done) {
     User.findOne({'email': email}, function (err, user) {
+      //console.log(user);
         if (err) {
             return done(err);
         }
         if (!user) {
-            return done(null, false);
+            return done(null, false,req.flash('message','Unknown user'));
         }
         bcrypt.compare(password,user.password,function(err, isMatch){
           if(err) throw err;
            if(!isMatch)
-           return done(null,false);
+           return done(null,false,req.flash('message','Invalid password'));
          else
         return done(null, user);
     });
   });
 }));
 
-router.get('/',ensureAuthenticated,function (req,res,next) {
+router.get('/',function (req,res,next) {
   //console.log(req.csrfToken());
   Product.find(function(err,docs) {
     if(err)
@@ -61,14 +65,14 @@ router.get('/',ensureAuthenticated,function (req,res,next) {
       for (var i = 0; i < docs.length; i += partitionSize) {
           docsPartition.push(docs.slice(i, i+partitionSize));
       }
-      res.render('shop/index',{title:'Shopping cart',products:docsPartition});
+      res.render('shop/index',{title:'Shopping cart',products:docsPartition,message:req.flash('message')});
     }
   });
 });
 
 router.get('/user/signup',function (req,res) {
   //console.log(req.csrfToken());
-  res.render('user/signup',{title:'Shopping cart',csrfToken:req.csrfToken()});
+  res.render('user/signup',{title:'Shopping cart',csrfToken:req.csrfToken(),message:req.flash('message')});
 })
 
 router.post('/user/signup',function (req,res) {
@@ -82,25 +86,29 @@ router.post('/user/signup',function (req,res) {
       });
       user.save(function (err) {
         if (err) {
-          var error = 'Something bad happened! Please try again.';
+          // req.flash('error','Something bad happened! Please try again.');
           if (err.code === 11000) {
-            error = 'That email is already taken, please try another.';
+            req.flash('message','That email is already taken, please try another.');
           }
-          res.render('/user/signup', { error: error });
+        //  console.log(req.flash('error'));
+          res.redirect('/user/signup');
           }
         else {
+          req.flash('message','Registered Successfully');
           res.redirect('/');
         }
-
       })
     });
 });
 });
+router.get('/user/profile',function(req,res){
+  res.render('user/profile',{title:'Shopping cart'});
+})
 router.get('/user/signin',function(req,res){
-  res.render('user/signin',{title:'Shopping cart',csrfToken:req.csrfToken()});
+res.render('user/signin',{title:'Shopping cart',csrfToken:req.csrfToken(),message: req.flash('message')});
 })
 router.post('/user/signin',
-passport.authenticate('local', {successRedirect:'/', failureRedirect:'/user/signin'}),
+passport.authenticate('local', {successRedirect:'/user/profile',failureFlash: true ,failureRedirect:'/user/signin'}),
 function(req, res) {
   res.redirect('/');
 });
