095f675a791c9a96da00f0e6fe3449f98d3ca30d
ganpt <ganpatchoudhary.28@gmail.com>
2017-05-22 23:41:00 +0530

    validation added using express-validator

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes/index');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash=require('connect-flash');
+var expressValidator = require('express-validator');
 var LocalStrategy = require('passport-local').Strategy;
 mongoose.connect('mongodb://localhost/ShoppingOwn');
 var app = express();
@@ -22,17 +23,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(expressValidator());
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-/*app.use(function(req,res,next){
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-//  console.log(res.locals.error);
-  next();
-})*/
 app.use('/', routes);
 app.listen(3000, function() {
   console.log("server listening at port 3000")
