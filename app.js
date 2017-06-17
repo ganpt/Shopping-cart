352de649c24c779c1ac2e68c7f6dbce52554e477
ganpt <ganpatchoudhary.28@gmail.com>
2017-06-17 16:16:46 +0530

    added code for cart

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var ejs = require('ejs');
 var path = require('path');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var expressLayouts = require('express-ejs-layouts');
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -18,7 +19,10 @@ var app = express();
 app.use(session({
   secret: 'secretkey',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 30*60})  //ttl in seconds
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
@@ -32,7 +36,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(function(req,res,next){
+  //console.log(req.session);
    res.locals.login=req.isAuthenticated();
+   res.locals.session=req.session;
    next();
 });
 app.use('/user',userRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-var express = require('express');
 var Product = require('../models/product');
+var express = require('express');
 var router = express.Router();
-
 router.get('/', function(req, res, next) {
   //console.log(req.csrfToken());
   Product.find(function(err, docs) {
@@ -21,5 +20,107 @@ router.get('/', function(req, res, next) {
       });
     }
   });
+});
+
+router.get('/add-to-cart/:id', function(req, res) {
+  var itemId = req.params.id;
+  Product.findById(itemId, function(err, item) {
+    if (err) {
+      res.send("Product not found error");
+    } else {
+      if (req.session.cart !== undefined) {
+        var itemObj = {};
+        itemObj.title = item.title;
+        itemObj.id = item.id;
+        itemObj.price = item.price;
+        itemObj.quantity = 1;
+        var itemPresent = 0;
+        var indexOfItem = -1;
+        var cartUpdate = req.session.cart;
+        for (var i = 0; i < cartUpdate.length; i++) {
+          if (cartUpdate[i].id === item.id) {
+            itemPresent = 1;
+            indexOfItem = i;
+            break;
+          }
+        }
+        if (itemPresent === 0) {
+          cartUpdate.push(itemObj);
+        } else {
+          cartUpdate[indexOfItem].price += itemObj.price;
+          cartUpdate[indexOfItem].quantity += 1;
+        }
+        req.session.cart = cartUpdate;
+      } else {
+        var newCart = [];
+        var itemObj = {};
+        itemObj.title = item.title;
+        itemObj.id = item.id;
+        itemObj.price = item.price;
+        itemObj.quantity = 1;
+        newCart.push(itemObj);
+        req.session.cart = newCart;
+      }
+    }
+    //console.log(req.session.cart);
+    var totalQuantity=0;
+    for(var i=0;i<req.session.cart.length;i++){
+      totalQuantity+=req.session.cart[i].quantity;
+    }
+    //  console.log(totalQuantity);
+    res.redirect('/');
+  })
+});
+router.get('/reduce/:id', function(req, res, next) {
+    var itemId = req.params.id;
+    var cart = req.session.cart;
+    var itemPrice=0;
+     for(var i=0;i<cart.length;i++)
+     {
+       if(cart[i].id===itemId)
+       {
+         if(cart[i].quantity===1)
+         cart.splice(i,1);
+         else {
+           itemPrice=cart[i].price/cart[i].quantity;
+           cart[i].quantity=cart[i].quantity-1;
+           cart[i].price=cart[i].price-itemPrice;
+         }
+         break;
+       }
+     }
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var itemId = req.params.id;
+  var cart = req.session.cart;
+  var itemPrice=0;
+   for(var i=0;i<cart.length;i++)
+   {
+     if(cart[i].id===itemId)
+     {
+       cart.splice(i,1);
+       break;
+     }
+   }
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+router.get('/shopping-cart', function(req, res, next) {
+    var totalPrice=0;var totalQuantity=0;
+    if (req.session.cart)
+    for(var i=0;i<req.session.cart.length;i++){
+      totalPrice+=req.session.cart[i].price;
+      totalQuantity+=req.session.cart[i].quantity;
+    }
+    var Cart=0;
+    if(totalQuantity===0)
+    Cart=null;
+    else {
+      Cart=req.session.cart;
+    }
+    res.render('shop/shopping-cart', {  title: 'Shopping cart',products:Cart ,totalPrice:totalPrice,totalQuantity:totalQuantity});
 });
 module.exports = router;
